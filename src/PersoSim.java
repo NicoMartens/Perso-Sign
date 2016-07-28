b790c3a068551c5251ff876581fd33b3f967a85c
NicoMartens <nico@railslove.com>
2016-07-28 09:23:43 +0200

    code cleanup

--- src/PersoSim.java
+++ src/PersoSim.java
@@ -100,24 +100,7 @@ public class PersoSim {
         // establish PACE Channel
         byte[] pace = establishPACEChannel(2, chat, null, null);
         println("PACE02: " + DatatypeConverter.printHexBinary(pace));
-        
-        
-        /*
-         * Output data of EstablishPACEChannel is defined as follows: 
-         * 
-         * 1 	USHORT 		Status 					Status codes of MSE:Set AT		
-         * 2 	USHORT		length_CardAccess 		Length of EF_CardAccess 
-         * 3 	BYTE[] 		EF_CardAccess 			Contents of EF.CardAccess as read from the chip 
-         * 4 	BYTE 		length_CARcurr 			Length of CARcurr 
-         * 5 	BYTE[] 		CARcurr 				Current Certificate Authority Reference 
-         * 6 	BYTE 		length_CARprev 			Length of CARprev 
-         * 7 	BYTE[] 		CARprev 				Previous Certificate Authority Reference 
-         * 8 	USHORT 		length_IDicc 			Length of IDicc 
-         * 9 	BYTE[] 		IDicc 					Ephemeral PACE public key of the IFD
-         * 
-         * Source: http://www.pcscworkgroup.com/specifications/files/pcsc10_v2.02.08_amd1.1.pdf Page 7
-         */
-        
+     
         
         //--------------- commands to select esign app --------------------------  
         
@@ -138,6 +121,9 @@ public class PersoSim {
         CommandAPDU verify = new CommandAPDU(new byte[] {0x00,0x20,0x00,(byte) 0x81,0x06,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF});
         byte[] verifyResponse = verifyPINDirect(verify);
         println("Verify response: " + DatatypeConverter.printHexBinary(verifyResponse));
+        
+        
+        //--------------- commands to execute signature --------------------------
         
         String data = "das ist ein kleines Beispiel fuer fuer daten die signiert werden sollen";
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -211,230 +197,221 @@ public class PersoSim {
 	}
 
 
-private static void printFeatures(byte[] features){
-	for(int i=0; i<features.length/6; i++){
-		print("Feature: " + translateFeatureNumber(DatatypeConverter.printHexBinary(new byte[] {features[i*6]})));
-		print("\t ControlNumber: " + Integer.parseInt(DatatypeConverter.printHexBinary(new byte[] {	features[i*6+2], 
-																									features[i*6+3], 
-																									features[i*6+4], 
-																									features[i*6+5]}),16));
-		println();
-	}
-}
-
-private static String translateFeatureNumber(String number){
-	switch(number){
-		case "01": return "FEATURE_VERIFY_PIN_START";
-		case "02": return "FEATURE_VERIFY_PIN_FINISH";
-		case "03": return "FEATURE_MODIFY_PIN_START";
-		case "04": return "FEATURE_MODIFY_PIN_FINISH";
-		case "05": return "FEATURE_GET_KEY_PRESSED";
-		case "06": return "FEATURE_VERIFY_PIN_DIRECT";
-		case "07": return "FEATURE_MODIFY_PIN_DIRECT";
-		case "08": return "FEATURE_MCT_READER_DIRECT";
-		case "09": return "FEATURE_MCT_UNIVERSAL";
-		case "0A": return "FEATURE_IFD_PIN_PROPERTIES";
-		case "0B": return "FEATURE_ABORT";
-		case "0C": return "FEATURE_SET_SPE_MESSAGE";
-		case "0D": return "FEATURE_VERIFY_PIN_DIRECT_APP_ID";
-		case "0E": return "FEATURE_MODIFY_PIN_DIRECT_APP_ID";
-		case "0F": return "FEATURE_WRITE_DISPLAY";
-		case "10": return "FEATURE_GET_KEY";
-		case "11": return "FEATURE_IFD_DISPLAY_PROPERTIES";
-		case "12": return "FEATURE_GET_TLV_PROPERTIES";
-		case "13": return "FEATURE_CCID_ESC_COMMAND";
-		case "20": return "FEATURE_EXECUTE_PACE"; 
-	}
-	return number + ": invalid feature number!";
-}
-
-private static boolean isWindows(){
-  String os_name = System.getProperty("os.name").toLowerCase();
-  if (os_name.indexOf("windows") > -1) return true;
-  return false;
-}
-
-private static int SCARD_CTL_CODE(int code){
-  int ioctl;
-  if (isWindows()){
-    ioctl = (0x31 << 16 | (code) << 2);
-  } 
-  else {
-    ioctl = 0x42000000 + (code);
-  }
-  return ioctl;
-}
-
-private static void printAPDU(ResponseAPDU r, String name){
-	print("\n");
-	print("APDU: " + name);
-	
-	print("\t");
-	print(r.toString());
-	
-	print("\t");
-	print("response data: \t");
-	for(byte b : r.getData()){
-		print(" " + b);
-    }
-	
-	print("\t");
-	print("data as text: \t");
-	String text = "";
-	for(byte b : r.getData()){
-		text += (char)b;
-	}
-	print(text);
-	print("\n");
-}
-
-private static void print(String s){
-	System.out.print(s);
-}
-
-private static void println(String s){
-	System.out.println(s);
-}
-
-private static void println(){
-	System.out.println();
-}
-
-public static byte[] joinByteArrays(List<byte[]> arrays){
-	
-	ArrayList<Byte> bytesAsList = new ArrayList<Byte>();
-	
-	for(byte[] a : arrays){
-		for(int i=0; i<a.length; i++){
-			bytesAsList.add(a[i]);
+	private static void printFeatures(byte[] features){
+		for(int i=0; i<features.length/6; i++){
+			print("Feature: " + translateFeatureNumber(DatatypeConverter.printHexBinary(new byte[] {features[i*6]})));
+			print("\t ControlNumber: " + Integer.parseInt(DatatypeConverter.printHexBinary(new byte[] {	features[i*6+2], 
+																										features[i*6+3], 
+																										features[i*6+4], 
+																										features[i*6+5]}),16));
+			println();
 		}
 	}
 	
-	byte[] bytes =  new byte[bytesAsList.size()];
-	for(int i=0; i<bytesAsList.size(); i++){
-		bytes[i] = bytesAsList.get(i);
+	private static String translateFeatureNumber(String number){
+		switch(number){
+			case "01": return "FEATURE_VERIFY_PIN_START";
+			case "02": return "FEATURE_VERIFY_PIN_FINISH";
+			case "03": return "FEATURE_MODIFY_PIN_START";
+			case "04": return "FEATURE_MODIFY_PIN_FINISH";
+			case "05": return "FEATURE_GET_KEY_PRESSED";
+			case "06": return "FEATURE_VERIFY_PIN_DIRECT";
+			case "07": return "FEATURE_MODIFY_PIN_DIRECT";
+			case "08": return "FEATURE_MCT_READER_DIRECT";
+			case "09": return "FEATURE_MCT_UNIVERSAL";
+			case "0A": return "FEATURE_IFD_PIN_PROPERTIES";
+			case "0B": return "FEATURE_ABORT";
+			case "0C": return "FEATURE_SET_SPE_MESSAGE";
+			case "0D": return "FEATURE_VERIFY_PIN_DIRECT_APP_ID";
+			case "0E": return "FEATURE_MODIFY_PIN_DIRECT_APP_ID";
+			case "0F": return "FEATURE_WRITE_DISPLAY";
+			case "10": return "FEATURE_GET_KEY";
+			case "11": return "FEATURE_IFD_DISPLAY_PROPERTIES";
+			case "12": return "FEATURE_GET_TLV_PROPERTIES";
+			case "13": return "FEATURE_CCID_ESC_COMMAND";
+			case "20": return "FEATURE_EXECUTE_PACE"; 
+		}
+		return number + ": invalid feature number!";
 	}
 	
-	return bytes;
-};
-
-public static byte[] establishPACEChannel(int pinid, byte[] chat, byte[] pin, byte[] certdesc) throws CardException {
-	//SCardControl
-	ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	bos.write(0x02); //Index of the PACE function: EstablishPACEChannel
-	bos.write(0x00); //Length template
-	bos.write(0x00); //Length template
-
-	//EstablishPACEChannelInputData
-	bos.write(pinid); 
-	if(chat != null) {
-		bos.write(chat.length);
-		bos.write(chat, 0, chat.length);
-	} else {
-		bos.write(0);
+	private static boolean isWindows(){
+	  String os_name = System.getProperty("os.name").toLowerCase();
+	  if (os_name.indexOf("windows") > -1) return true;
+	  return false;
 	}
-	if(pin != null) {
-		bos.write(pin.length);
-		bos.write(pin, 0, pin.length);
-	} else {
-		bos.write(0);
+	
+	private static int SCARD_CTL_CODE(int code){
+	  int ioctl;
+	  if (isWindows()){
+	    ioctl = (0x31 << 16 | (code) << 2);
+	  } 
+	  else {
+	    ioctl = 0x42000000 + (code);
+	  }
+	  return ioctl;
 	}
-	if(certdesc != null) {
-		bos.write((byte) (certdesc.length & 0xFF));
-		bos.write((byte) (certdesc.length >> 8));
-		bos.write(certdesc, 0, certdesc.length);
-	} else {
-		bos.write(0);
-		bos.write(0);
+	
+	private static void printAPDU(ResponseAPDU r, String name){
+		print("\n");
+		print("APDU: " + name);
+		
+		print("\t");
+		print(r.toString());
+		
+		print("\t");
+		print("response data: \t");
+		for(byte b : r.getData()){
+			print(" " + DatatypeConverter.printHexBinary(new byte[] {b}));
+	    }
 	}
-
-	int inputLength = bos.size() - 3;
-
-	byte[] inputData = bos.toByteArray();
-	//Writing the length of the EstablishPACEChannelInputData into the template
-	inputData[1] = (byte) (inputLength & 0xFF);
-	inputData[2] = (byte) (inputLength >> 8);
-
-	int PACEControlCode = getFeatureControlCode(FEATURE_EXECUTE_PACE);
-	byte[] rpc = card.transmitControlCommand(PACEControlCode, inputData);
-
-	long result =  extractLongFromByteArray(rpc, 0, 4);
-	System.out.println("PACE result: " + Long.toHexString(result));
-
-	return rpc;
-}
-
-public static long getReadersPACECapabilities() throws CardException {
-	int PACEControlCode = getFeatureControlCode(FEATURE_EXECUTE_PACE);
-
-	//SCardControl
-	ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	bos.write(0x01); //Index of the PACE function
-	bos.write(0x00); //Two byte length field
-	bos.write(0x00);
-	byte[] commandBlock = bos.toByteArray();
-
-	byte[] rpc = card.transmitControlCommand(PACEControlCode, commandBlock);
-	long result = extractLongFromByteArray(rpc, 0, 4);
-	if(result == 0) {
-		long outputData = extractLongFromByteArray(rpc, 6, 1);
-		return outputData;
+	
+	private static void print(String s){
+		System.out.print(s);
 	}
-	else{
+	
+	private static void println(String s){
+		System.out.println(s);
+	}
+	
+	private static void println(){
+		System.out.println();
+	}
+	
+	public static byte[] joinByteArrays(List<byte[]> arrays){
+		
+		ArrayList<Byte> bytesAsList = new ArrayList<Byte>();
+		
+		for(byte[] a : arrays){
+			for(int i=0; i<a.length; i++){
+				bytesAsList.add(a[i]);
+			}
+		}
+		
+		byte[] bytes =  new byte[bytesAsList.size()];
+		for(int i=0; i<bytesAsList.size(); i++){
+			bytes[i] = bytesAsList.get(i);
+		}
+		
+		return bytes;
+	};
+	
+	public static byte[] establishPACEChannel(int pinid, byte[] chat, byte[] pin, byte[] certdesc) throws CardException {
+		//SCardControl
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		bos.write(0x02); //Index of the PACE function: EstablishPACEChannel
+		bos.write(0x00); //Length template
+		bos.write(0x00); //Length template
+	
+		//EstablishPACEChannelInputData
+		bos.write(pinid); 
+		if(chat != null) {
+			bos.write(chat.length);
+			bos.write(chat, 0, chat.length);
+		} else {
+			bos.write(0);
+		}
+		if(pin != null) {
+			bos.write(pin.length);
+			bos.write(pin, 0, pin.length);
+		} else {
+			bos.write(0);
+		}
+		if(certdesc != null) {
+			bos.write((byte) (certdesc.length & 0xFF));
+			bos.write((byte) (certdesc.length >> 8));
+			bos.write(certdesc, 0, certdesc.length);
+		} else {
+			bos.write(0);
+			bos.write(0);
+		}
+	
+		int inputLength = bos.size() - 3;
+	
+		byte[] inputData = bos.toByteArray();
+		//Writing the length of the EstablishPACEChannelInputData into the template
+		inputData[1] = (byte) (inputLength & 0xFF);
+		inputData[2] = (byte) (inputLength >> 8);
+	
+		int PACEControlCode = getFeatureControlCode(FEATURE_EXECUTE_PACE);
+		byte[] rpc = card.transmitControlCommand(PACEControlCode, inputData);
+	
+		long result =  extractLongFromByteArray(rpc, 0, 4);
+		System.out.println("PACE result: " + Long.toHexString(result));
+	
+		return rpc;
+	}
+	
+	public static long getReadersPACECapabilities() throws CardException {
+		int PACEControlCode = getFeatureControlCode(FEATURE_EXECUTE_PACE);
+	
+		//SCardControl
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		bos.write(0x01); //Index of the PACE function
+		bos.write(0x00); //Two byte length field
+		bos.write(0x00);
+		byte[] commandBlock = bos.toByteArray();
+	
+		byte[] rpc = card.transmitControlCommand(PACEControlCode, commandBlock);
+		long result = extractLongFromByteArray(rpc, 0, 4);
+		if(result == 0) {
+			long outputData = extractLongFromByteArray(rpc, 6, 1);
+			return outputData;
+		}
+		else{
+			return -1;
+		}	
+	}
+	
+	public static long extractLongFromByteArray(byte[] buffer, int offset, int length) {
+		if((offset + length) > buffer.length) {
+			throw new IndexOutOfBoundsException("Length exceeds buffer size");
+		}
+		if(length > 8) {
+			throw new IllegalArgumentException("Cannot decode more than 8 byte");
+		}
+		
+		long c = 0;
+		while (length-- > 0) {
+			c <<= 8;
+			c |= buffer[offset + length] & 0xFF;
+		}
+		return c;
+	}
+	
+	public static int getFeatureControlCode(int feature) {
+		byte[] features;
+	
+		int ioctl = (0x31 << 16 | (3400) << 2);
+	
+		byte[] empty = {};
+	
+		try {
+			features = card.transmitControlCommand(ioctl, empty);
+		}
+		catch(CardException e) {
+			return -1;
+		}
+	
+		int i = 0;
+	
+		while(i < features.length) {
+			if (features[i] == feature) {
+				int c = 0;
+				i += 2;				
+				for (int l = 0; (i < features.length) && (l < 4); i++, l++) {
+					c <<= 8;
+					c |= features[i] & 0xFF;
+				}
+	
+				return c;
+	
+			} else {
+				i += 6; // skip six bytes
+			}
+		}
+	
 		return -1;
 	}	
-}
-
-public static long extractLongFromByteArray(byte[] buffer, int offset, int length) {
-	if((offset + length) > buffer.length) {
-		throw new IndexOutOfBoundsException("Length exceeds buffer size");
-	}
-	if(length > 8) {
-		throw new IllegalArgumentException("Cannot decode more than 8 byte");
-	}
-	
-	long c = 0;
-	while (length-- > 0) {
-		c <<= 8;
-		c |= buffer[offset + length] & 0xFF;
-	}
-	return c;
-}
-
-public static int getFeatureControlCode(int feature) {
-	byte[] features;
-
-	int ioctl = (0x31 << 16 | (3400) << 2);
-
-	byte[] empty = {};
-
-	try {
-		features = card.transmitControlCommand(ioctl, empty);
-	}
-	catch(CardException e) {
-		return -1;
-	}
-
-	int i = 0;
-
-	while(i < features.length) {
-		if (features[i] == feature) {
-			int c = 0;
-			i += 2;				
-			for (int l = 0; (i < features.length) && (l < 4); i++, l++) {
-				c <<= 8;
-				c |= features[i] & 0xFF;
-			}
-
-			return c;
-
-		} else {
-			i += 6; // skip six bytes
-		}
-	}
-
-	return -1;
-}	
 
 }
 
